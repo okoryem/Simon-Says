4a16e3b429389a95effe6b2f021702bfa0836426
okoryem <157544408+okoryem@users.noreply.github.com>
2024-12-17 22:35:22 -0500

    Create project.h

---
+++ project.h
@@ -0,0 +1,40 @@
+#define _GUN_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "sense.h"
+#include <time.h>
+#include <linux/input.h>
+
+void openDisplay(void);
+
+void countDown(void);
+
+void closeDisplay(void);
+
+void clearDisplay(void);
+
+void drawSymbol(int nextMove);
+
+int checkChoice(int nextMove);
+
+int checkTilt(int nextMove);
+
+int checkCorrect(int isCorrect);
+
+void openTilt(void);
+
+void openInput(void);
+
+void closeTilt(void);
+
+void closeInput(void);
+
+void checkJoyInput(void (*callback)(unsigned int code), int delay);
+
+void interrupt_handler(int sig);
+
+void handler(unsigned int code);
+
+int randNum(void);
